dd9e931c514533f25b89e3a0d42a54e270ad923a
Lazarus99 <63731825+Lazarus99@users.noreply.github.com>
2025-10-03 18:13:49 -0400

    Updated endpoint message

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello from Docker + npm test!");
+  res.send("Hello, CI Pipeline!");
 });
 
 app.listen(3000, () => console.log("Server running on port 3000"));
